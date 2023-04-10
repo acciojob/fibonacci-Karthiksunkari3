d9023c368ac88a031004a330e4a0a29ec26c3025
Karthik Sunkari <84893245+Karthiksunkari3@users.noreply.github.com>
2023-04-11 01:00:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
 // your code here
+	let n1=0,n2=1,n3,;
+	console.log(n1+" "+n2);
+	if(let i=0;i<num;++i){
+      n3=n1+n2;
+		console.log(" "+n3);
+		n1=n2;
+		n2=n3;
+	}
 }
 
 module.exports = fibonacci;
